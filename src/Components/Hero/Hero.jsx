69574b55fb188891907a18cddf1b1127bffa90fc
SHUVO <im.computer.engineeer@gamil.com>
2025-09-18 17:27:04 +0600

    Next portfolio Navbar complete

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full mx-auto px-4 md:px-10">
+      <h1 className="text-white text-4xl font-bold ">This is hero section</h1>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,188 @@
+"use client";
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openMenu = () => setIsOpen(true);
+  const closeMenu = () => setIsOpen(false);
+
+  return (
+    <div className="w-screen h-full ">
+      <div className="container mx-auto relative md:px-0 px-4 md:my-4   bg-cyan-950  rounded-xl overflow-hidden">
+        {/* Nav */}
+        <nav
+          className="group bg-cyan-950 shadow-lg w-full z-50 
+             relative group-hover:fixed group-hover:top-0 group-hover:left-0"
+        >
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between items-center h-16 ">
+              {/*------------- Animate Logo ------------- */}
+              <div className="flex-shrink-0">
+                <a
+                  href="#"
+                  className="lg:text-xl md:text-lg text-sm font-bold text-cyan-100  capitalize"
+                >
+                  <span
+                    className="px-1 bg-lime-400 dark:bg-green-600
+                   rounded-full animate-[ping_1.5s_linear_infinite]"
+                  >
+                    S
+                  </span>
+                  <span
+                    className="px-1 bg-green-400 dark:bg-red-600
+                   rounded-full animate-[ping_2s_linear_infinite]"
+                  >
+                    H
+                  </span>
+                  <span
+                    className="px-1 bg-green-400 dark:bg-yellow-400
+                   rounded-full animate-[ping_2.1s_linear_infinite]"
+                  >
+                    U
+                  </span>
+                  <span
+                    className="px-1 bg-green-400 dark:bg-sky-600
+                   rounded-full animate-[ping_2.21s_linear_infinite]"
+                  >
+                    V
+                  </span>
+                  <span
+                    className="px-1 bg-green-400 dark:bg-orange-500
+                   rounded-full animate-[ping_2.3s_linear_infinite]"
+                  >
+                    O
+                  </span>
+                </a>
+              </div>
+
+              {/*------------ Desktop inside Text --------- */}
+              {/* Desktop Menu */}
+              <div className="hidden md:flex space-x-8 text-stone-300 ">
+                <a href="#about" className="text-green-400 dark:text-green-600">
+                  Home
+                </a>
+                <a
+                  href="#what-i-do"
+                  className="hover:text-green-400 dark:hover:text-green-600"
+                >
+                  What I Do
+                </a>
+                <a
+                  href="#testimonials"
+                  className="hover:text-green-400 dark:hover:text-green-600"
+                >
+                  Testimonials
+                </a>
+                <a
+                  href="#clients"
+                  className="hover:text-green-400 dark:hover:text-green-600"
+                >
+                  Clients
+                </a>
+                <a
+                  href="#fun-facts"
+                  className="hover:text-green-400 dark:hover:text-green-600"
+                >
+                  Fun Facts
+                </a>
+              </div>
+
+              {/*------------- Mobile Menu Button ---------- */}
+              <div className="md:hidden flex items-center">
+                <button
+                  onClick={openMenu}
+                  className="text-green-400 dark:text-green-600 hover:text-green-600
+                   focus:outline-none"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* Mobile Menu */}
+          {isOpen && (
+            <div
+              onClick={(e) => e.target === e.currentTarget && closeMenu()}
+              className="absolute inset-0   bg-opacity-50 z-50"
+            >
+              <div
+                className="fixed inset-y-0 right-0 bg-[#000000] 
+        bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] 
+        bg-[size:20px_20px]  w-52 p-6 transform transition-transform"
+              >
+                <button
+                  onClick={closeMenu}
+                  className="text-green-400 dark:text-green-600 hover:text-green-600 focus:outline-none mb-4"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+                <nav className="space-y-4 text-stone-300 ">
+                  <a
+                    href="#about"
+                    className="block text-green-400 dark:text-green-600"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="#what-i-do"
+                    className="block hover:text-green-400 dark:hover:text-green-600"
+                  >
+                    What I Do
+                  </a>
+                  <a
+                    href="#testimonials"
+                    className="block hover:text-green-400 dark:hover:text-green-600"
+                  >
+                    Testimonials
+                  </a>
+                  <a
+                    href="#clients"
+                    className="block hover:text-green-400 dark:hover:text-green-600"
+                  >
+                    Clients
+                  </a>
+                  <a
+                    href="#fun-facts"
+                    className="block hover:text-green-400 dark:hover:text-green-600"
+                  >
+                    Fun Facts
+                  </a>
+                </nav>
+              </div>
+            </div>
+          )}
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
